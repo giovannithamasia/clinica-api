5f085ae9935f929cb5be37be6a390f3e353f3325
GIOVANNI THAMASIA <giovanni_thamasia@estudante.sesisenai.org.br>
2026-04-27 20:32:13 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/com/senai/clinicaApi/ClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.clinicaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
