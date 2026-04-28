8a6eb8750dcf1fc7453bd1efa077bc47cd2da7ff
CLEITON BERTOLDI <cleiton_bertoldi@estudante.sc.senai.br>
2026-04-28 19:03:23 -0300

    feat: implementa verificações de existência de email no repositório

--- src/main/java/com/senai/clinicaApi/repositories/PacienteRepository.java
+++ src/main/java/com/senai/clinicaApi/repositories/PacienteRepository.java
@@ -2,11 +2,21 @@ package com.senai.clinicaApi.repositories;
 
 import com.senai.clinicaApi.entities.PacienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface PacienteRepository extends JpaRepository <PacienteEntity, Long> {
 
    Optional<PacienteEntity> findByEmail(String email);
+
+   @Modifying
+   @Transactional
+   void deleteByEmail(String email);
+
+   boolean existsByEmail(String email);
+
+   boolean existsByEmailAndIdNot(String email, Long id);
 
 }
