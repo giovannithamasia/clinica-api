1cc77fab5599f4c9175ba29d784a0511d74d1bd9
CLEITON BERTOLDI <cleiton_bertoldi@estudante.sc.senai.br>
2026-04-28 19:06:58 -0300

    feat: adiciona endpoints faltantes

--- src/main/java/com/senai/clinicaApi/controllers/PacienteController.java
+++ src/main/java/com/senai/clinicaApi/controllers/PacienteController.java
@@ -24,17 +24,52 @@ public class PacienteController {
     }
     @PostMapping
     public ResponseEntity<String>criarPaciente(@RequestBody @Valid PacienteDto pacienteDto){
-        boolean sucesso = pacienteService.inserirPaciente(pacienteDto);
-        if (sucesso) {
-            return ResponseEntity.ok("Paciente inserido com sucesso");
-        } else {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Já existe paciente");
-        }
+       try {
+              pacienteService.inserirPaciente(pacienteDto);
+              return ResponseEntity.status(HttpStatus.CREATED).body("Paciente criado com sucesso");
+       }catch (RuntimeException e) {
+           return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+       }
     }
     @GetMapping
     public ResponseEntity<List<PacienteRespostaDto>> listar() {
         List<PacienteRespostaDto> lista = pacienteService.obterPacientes();
+        if (lista.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(lista);
     }
 
+    @GetMapping("/{email}")
+    public ResponseEntity<Object> obterPacientePorEmail(@PathVariable String email) {
+        try {
+            PacienteRespostaDto paciente = pacienteService.obterPacientePorEmail(email);
+            return ResponseEntity.ok(paciente);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{email}")
+    public ResponseEntity<String>atualizarPaciente(@PathVariable String email, @RequestBody @Valid PacienteDto pacienteDto){
+
+       try {
+           pacienteService.atualizarPaciente(email, pacienteDto);
+              return ResponseEntity.ok("Paciente atualizado com sucesso");
+       }catch (IllegalArgumentException e){
+           return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+
+    }catch (RuntimeException e){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+       }
+    }
+    @DeleteMapping("{email}")
+    public ResponseEntity<String>excluirPaciente(@PathVariable String email){
+        try {
+            pacienteService.excluirPaciente(email);
+            return ResponseEntity.ok("Paciente excluído com sucesso");
+        }catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
