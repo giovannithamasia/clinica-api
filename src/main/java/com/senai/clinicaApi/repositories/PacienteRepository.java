ec9ae86a35dd942ac12e6260f8f0ade92383187c
CLEITON BERTOLDI <cleiton_bertoldi@estudante.sc.senai.br>
2026-04-27 21:16:07 -0300

    feat: adiciona query para buscar paciente pelo email

--- src/main/java/com/senai/clinicaApi/repositories/PacienteRepository.java
+++ src/main/java/com/senai/clinicaApi/repositories/PacienteRepository.java
@@ -3,6 +3,10 @@ package com.senai.clinicaApi.repositories;
 import com.senai.clinicaApi.entities.PacienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PacienteRepository extends JpaRepository <PacienteEntity, Long> {
+
+   Optional<PacienteEntity> findByEmail(String email);
 
 }
